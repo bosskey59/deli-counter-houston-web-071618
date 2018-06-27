fde9fe32e4e561ebbdf2084032e8885b84b0ddc4
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-06-27 21:25:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,13 +7,13 @@ def take_a_number(line, name)
 end
 
 def line(line)
-  # display_line =[]
+  display_line =""
   if line.length==0
     puts"The line is currently empty."
   else
     line.each_with_index do |val, index|
       display_line.concat(" #{index+1}. #{val}")
     end
-    puts "The line is currently:#{display_line.to_s()}"
+    puts "The line is currently:#{display_line}"
   end
 end
